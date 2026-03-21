87e63e1d09a7531b622664564aa4813a7fd77860
Hygge-star <1467306547@qq.com>
2026-03-21 16:05:36 +0800

    Delete test_tb.py

--- test_tb.py
+++
@@ -1,9 +0,0 @@
-from torch.utils.tensorboard import SummaryWriter
-
-writer=SummaryWriter("log")
-#writer.add_image()
-#y=2x
-for i in range(100):
-    writer.add_scalar("y=2x", 2*i, global_step=i)
-
-writer.close()
